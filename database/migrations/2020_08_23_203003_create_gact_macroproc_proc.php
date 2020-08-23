6296de0c47615b77763b1960a3f35043d79f6eee
titoelpropio77 <titoelpropio77@gmail.com>
2020-08-23 17:02:54 -0400

    Nuevos migration

---
+++ database/migrations/2020_08_23_203003_create_gact_macroproc_proc.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGactMacroprocProc extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    private  $prefijo = "proc_";
+    public function up()
+    {
+        
+        Schema::create('gact_macroprocesos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('macpro_nombre',30);
+            $table->string('macpro_descripcion', 250);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+       
+        Schema::create('gact_procesos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string($this->prefijo.'nombre',30);
+            $table->enum($this->prefijo.'grado_automatizacion', ['Semiatuomatizado', 'Automatizado']);
+            $table->enum($this->prefijo.'grado_descentralizacion', ['Centralizado', 'Desentralizado']);
+            $table->enum($this->prefijo.'periodo_ejecucion', ['Diario', 'Semanal', 'Mensual', 'Anual']);
+            $table->string($this->prefijo.'reponsable_ejecucion',30);
+            $table->string($this->prefijo.'reponsable_revision',30);
+            $table->unsignedBigInteger($this->prefijo.'macroproceso_id');
+            $table->foreign($this->prefijo.'macroproceso_id')->references('id')->on('gact_macroprocesos');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gact_macroprocesos');
+        Schema::dropIfExists('gact_procesos');
+    }
+}
